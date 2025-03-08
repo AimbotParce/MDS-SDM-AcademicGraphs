cec555f144cc285133c38cedab3cc65c26d084d9
AimbotParce <marcparcerisa@gmail.com>
2025-03-08 23:08:52 +0100

    Play a bit with the SemanticScholar API

--- src/A/ingestion.py
+++ src/A/ingestion.py
@@ -1,28 +1,42 @@
 import requests
-import os 
-import urllib
+import urllib.request
 import pprint
 
 
 if __name__ == "__main__":
-    # Get info about the latest release
-    latest_release = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest").json()
-    print(latest_release['README'])
-    print(latest_release['release_id'])
+    from dotenv import load_dotenv
+    import os
 
-    # Get info about past releases
-    dataset_ids = requests.get("http://api.semanticscholar.org/datasets/v1/release").json()
-    earliest_release = requests.get(f"http://api.semanticscholar.org/datasets/v1/release/{dataset_ids[0]}").json()
+    load_dotenv()
+    api_url = os.getenv("S2_API_URL", "http://api.semanticscholar.org/datasets/v1")
+
+
+    # Get info about the latest release
+    releases = requests.get(f"{api_url}/release").json()
+    # Releases is a list of strings, each string is a release ID (Which corresponds to a date of release)
+    latest_release = requests.get(f"{api_url}/release/latest").json()
+    latest_release_id = latest_release['release_id']
+    # Latest_release is a dictionary with keys 'release_id', 'datasets', 'README'.
+    # 'datasets' is a list of dictionaries with keys 'name', 'description', 'README'.
+    # print(latest_release['README'])
+    print("Latest Release:", latest_release_id)
+
+    # Earliest release
+    earliest_release_id = min(releases)
+    print("Earliest Release:", earliest_release_id)
+    earliest_release = requests.get(f"{api_url}/release/{earliest_release_id}").json()
 
     # Print names of datasets in the release
-    print("\n".join(d['name'] for d in latest_release['datasets']))
+    release_id = latest_release['release_id']
+    print("Datasets in Latest Release:", ", ".join(d['name'] for d in latest_release['datasets']))
 
-    # Print README for one of the datasets
-    print(latest_release['datasets'][2]['README'])
+    papers_dataset = list(filter(lambda x: x['name'] == "papers", latest_release['datasets']))[0]
+    print("Papers Dataset:")
+    print("\n".join(map(lambda x: "    " + x, str.splitlines(papers_dataset['description']))))
 
     # Get info about the papers dataset
-    papers = requests.get("http://api.semanticscholar.org/datasets/v1/release/latest/dataset/papers",
-                        headers={'X-API-KEY':os.getenv("S2_API_KEY")}).json()
-
+    papers = requests.get(f"{api_url}/release/{latest_release_id}/dataset/papers", 
+                          headers={'X-API-KEY':os.getenv("S2_API_KEY")}).json()
+    
     # Download the first part of the dataset
     urllib.request.urlretrieve(papers['files'][0], "papers-part0.jsonl.gz")
