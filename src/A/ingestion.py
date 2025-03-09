89a8e5c4ab2a37f94bfb326d8934c77685ca6838
AimbotParce <marcparcerisa@gmail.com>
2025-03-09 13:39:55 +0100

    Data downloading

--- src/A/ingestion.py
+++ src/A/ingestion.py
@@ -1,7 +1,53 @@
+from pathlib import Path
 import requests
 import urllib.request
-import pprint
+from tqdm import tqdm
 
+class SemanticScholarAPI:
+    def __init__(self, api_url:str, api_key:str=None):
+        self.api_url = api_url
+        self.api_key = api_key
+
+    def get(self, endpoint:str):
+        return requests.get(f"{self.api_url}/{endpoint}", headers={'X-API-KEY':self.api_key}).json()
+        
+
+    def downloadDatasetFiles(self, release_id:str, dataset_name:str, output_dir:Path, max_files:int=None, progressbar:bool=True):
+        output_dir = Path(output_dir)
+        dataset = self.get(f"release/{release_id}/dataset/{dataset_name}")
+        files = dataset['files']
+        zeros = len(str(len(files)))
+        for i, file in enumerate(files, 1):
+            if max_files and i > max_files:
+                break
+            output_file = output_dir / f"rel-{release_id}-{dataset_name}-{i:0{zeros}d}.jsonl.gz"
+            if output_file.exists():
+                print(f"WARNING: File {output_file} already exists. Skipping.")
+                continue
+            else:
+                if progressbar:
+                    bar = tqdm(unit="B", unit_scale=True, leave=False, desc=f"Downloading {output_file.name}", ascii=" ▏▎▍▌▋▊▉█")
+                    def report(block_num, block_size, total_size):
+                        bar.total = total_size
+                        bar.update(block_num * block_size - bar.n)
+                else:
+                    print(f"Downloading {output_file}")
+                    report = None
+                try:
+                    urllib.request.urlretrieve(file, output_file, reporthook=report)
+                except KeyboardInterrupt:
+                    print("Download interrupted. Cleaning up.")
+                    output_file.unlink()
+                    raise KeyboardInterrupt
+                except Exception as e:
+                    print(f"ERROR: {e}")
+                    output_file.unlink()
+                    break
+                finally:
+                    if progressbar:
+                        bar.close()
+                        print(f"Downloaded {output_file}")
+        print("Download complete.")
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
@@ -9,34 +55,35 @@ if __name__ == "__main__":
 
     load_dotenv()
     api_url = os.getenv("S2_API_URL", "http://api.semanticscholar.org/datasets/v1")
+    api_key = os.getenv("S2_API_KEY")
+    api = SemanticScholarAPI(api_url, api_key)
 
 
-    # Get info about the latest release
-    releases = requests.get(f"{api_url}/release").json()
+    # Get all releases
+    release_ids = api.get("release")
     # Releases is a list of strings, each string is a release ID (Which corresponds to a date of release)
-    latest_release = requests.get(f"{api_url}/release/latest").json()
-    latest_release_id = latest_release['release_id']
-    # Latest_release is a dictionary with keys 'release_id', 'datasets', 'README'.
-    # 'datasets' is a list of dictionaries with keys 'name', 'description', 'README'.
-    # print(latest_release['README'])
+    
+    print("Earliest Release:", min(release_ids))
+    latest_release_id = max(release_ids)
     print("Latest Release:", latest_release_id)
 
-    # Earliest release
-    earliest_release_id = min(releases)
-    print("Earliest Release:", earliest_release_id)
-    earliest_release = requests.get(f"{api_url}/release/{earliest_release_id}").json()
 
-    # Print names of datasets in the release
-    release_id = latest_release['release_id']
+    # Get all information from latest release
+    latest_release = api.get(f"release/{latest_release_id}")
+    # Release information is a dictionary with keys 'release_id', 'datasets', 'README'.
+    # 'datasets' is a list of dictionaries with keys 'name', 'description', 'README'.
     print("Datasets in Latest Release:", ", ".join(d['name'] for d in latest_release['datasets']))
 
-    papers_dataset = list(filter(lambda x: x['name'] == "papers", latest_release['datasets']))[0]
-    print("Papers Dataset:")
-    print("\n".join(map(lambda x: "    " + x, str.splitlines(papers_dataset['description']))))
+    # We're interested in the papers dataset
 
     # Get info about the papers dataset
-    papers = requests.get(f"{api_url}/release/{latest_release_id}/dataset/papers", 
-                          headers={'X-API-KEY':os.getenv("S2_API_KEY")}).json()
+    papers_dataset = api.get(f"release/{latest_release_id}/dataset/papers")
+    # Papers dataset is a dictionary with keys 'name', 'description', 'README' and 'files'
+
+    print("Papers Dataset:")
+    print("\n".join(map(lambda x: "    " + x, str.splitlines(papers_dataset['description']))))
+    papers_files = papers_dataset['files']
+    print("Papers Dataset Files:", len(papers_files))
     
-    # Download the first part of the dataset
-    urllib.request.urlretrieve(papers['files'][0], "papers-part0.jsonl.gz")
+    # Download the papers dataset files
+    api.downloadDatasetFiles(latest_release_id, 'papers', 'data')
