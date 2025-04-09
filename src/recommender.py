385b9fc37a17adeffc079d9b3ed6f4ed2c5bc0c3
eZWALT <waltertheshadow333@gmail.com>
2025-04-09 13:19:06 +0200

    Added initial version of recommendation system

--- src/recommender.py
+++ src/recommender.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 import json
 from loguru import logger
+from typing import List, Dict, Optional
+import re
 
 load_dotenv()
 neo4j_username = os.getenv("NEO4J_USER")
@@ -130,42 +132,126 @@ def fetch_papers_from_neo4j():
 def run_query(tx, query):
     tx.run(query)
 
+def to_camel_case(name: str) -> str:
+    parts = re.split(r'[\s_-]+', name)
+    return ''.join(word.capitalize() for word in parts) 
 
-def step1_recsys_define_community(tx):
-    query = """
+#Encapsulate LABEL logic in these functions to avoid hardcoding and low locality of behaviour
+def get_community_venue_label(community_name: str):
+    return to_camel_case(community_name) + "Venue"
+
+def get_community_toppaper_label(community_name: str):
+    return f"Top{community_name}Paper"
+
+def get_community_reviewer_label(community_name: str):
+    return f"{community_name}Reviewer"
+
+def get_community_guru_label(community_name: str):
+    return f"{community_name}Guru"
+
+# This statement generates:
+# 1. A community
+# 2. Keywords related to that community
+# 3. Edges from community<->keyword
+def step1_recsys_define_community(tx,
+    community: str = "Database",
+    keywords: List[str] = [
+      "data management", "indexing", "data modeling",
+      "big data", "data processing", "data storage", "data querying"
+    ])->None:
     
+    keywords_cypher = "[" + ", ".join(f'"{kw}"' for kw in keywords) + "]"
+
+    query = f"""
+    MERGE (c:Community {{name: "{community}"}})
+    WITH c, {keywords_cypher} AS keywords
+    UNWIND keywords as kw
+    MERGE (k: Keyword {{name: kw}})
+    MERGE (c)-[:HasKeyword]->(k)
     """
     run_query(tx, query)
 
 
-def step2_recsys_label_venues(tx):
-    query = """
+# This statement searches the venues related to that community
+# through a simple percentage (papers that have any keyword related vs rest in venue)
+# and labels that venue as a community-venue
+def step2_recsys_label_venues(tx,
+    community: str = "Database",
+    percentage: float = 0.9):
+    
+    query = f"""
+    CALL {{
+        MATCH (c:Community {{name: "{community}}})-[:HasKeyword]->(k:Keyword)       // Get community and its keywords
+        MATCH (v)<-[:PublishedIn]-(p:Publication)-[:HasKeyword]->(k)                // Get venues and keywords of a paper
+        WHERE v:Proceedings OR v:JournalVolume OR v.OtherPublicationVenue
+        WITH v, COUNT(DISTINCT p) as related_papers
+        MATCH (v)<-[:PublishedIn]-(all:Publication) // Get all papers of venue v 
+        WITH v, related_papers, COUNT(all) as total_papers
+        WHERE total_papers > 0 AND (related_papers * 1.0 / total_papers) >= {percentage}
+        SET v:{get_community_venue_label(community)}                                // Tag this venue for this community
+        
+    }}
+    """
+    run_query(tx, query)
+
+# Get the top 100 papers of those community-venues that have any keyword
+# related to the community. The strategy is to gather those with the maximum citation count
+# Then, those top papers will be labeled as Top paper for that specific community (property)
+def step3_recsys_rank_top100_papers(tx,     
+    community: str = "Database",
+    top_n: int = 100):
+    
+    query = f"""
+    MATCH (p:Publication)-[:PublishedIn]->(v)
+    WHERE v:{get_community_venue_label(community)}
+    MATCH (citing:Publication)-[:Cites]->(p) // Get citations with keywords related to the community
+    MATCH (citing)-[:HasKeyword]->(k)        
+    MATCH (k)<-[:HasKeyword]-(c:Community {{name: "{community}"}})
+    WITH p, COUNT(DISTINCT citing) AS citations
+    ORDER BY citations DESC
+    LIMIT {top_n}
+    SET p:{get_community_toppaper_label(community)}
+    """
+    run_query(tx, query)
+
+# Label the potential reviewers out of the top papers by 
+def step4_recsys_label_reviewers_and_gurus(tx,
+    community: str = "Database",
+    min_guru_top_papers: int = 2):
+        
+    query = f"""
+    MATCH (a:Author)-[:Authored]->(p:{get_community_toppaper_label(community)})
+    WITH a, COUNT(p), AS top_papers
+    SET a: {get_community_reviewer_label(community)} // Recommend community reviewer
+    WITH a, top_papers
+    WHERE top_papers >= {min_guru_top_papers}
+    SET a:{get_community_guru_label(community)} // Recommend gurus for that community
     
     """
     run_query(tx, query)
 
+def undo_recsys_modifications(tx, steps: List[int] = [0,1,2,3,4]):
+    undo_queries = {
+        0: "MATCH (c:Community {name: 'Database'})-[r:HAS_KEYWORD]->() DELETE r",
+        1: "MATCH (v:DatabaseVenue) REMOVE v:DatabaseVenue",
+        2: "MATCH (p:TopDatabasePaper) REMOVE p:TopDatabasePaper",
+        3: "MATCH (a:Reviewer) REMOVE a:Reviewer",
+        4: "MATCH (a:Guru) REMOVE a:Guru",
+    }
 
-def step3_recsys_rank_top100_papers(tx):
-    query = """
-    
-    """
-    run_query(tx, query)
-
-
-def step4_recsys_label_reviewers_and_gurus(tx):
-    query = """
-    
-    """
-    run_query(tx, query)
-
+    for step in steps:
+        query = undo_queries.get(step)
+        if query:
+            run_query(tx, query)
 
 def execute_recommendation_algorithm(driver: GraphDatabase.driver = driver):
     with driver.session() as session:
         # All these operations are effectiely upserts
         session.execute_write(step1_recsys_define_community)
-        session.execute_write(step2_recsys_label_venues)
-        session.execute_write(step3_recsys_rank_top100_papers)
-        session.execute_write(step4_recsys_label_reviewers_and_gurus)
+        # session.execute_write(step2_recsys_label_venues)
+        # session.execute_write(step3_recsys_rank_top100_papers)
+        # session.execute_write(step4_recsys_label_reviewers_and_gurus)
+        
 
 
 if __name__ == "__main__":
